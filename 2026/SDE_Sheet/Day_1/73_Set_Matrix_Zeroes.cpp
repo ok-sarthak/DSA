e84150650fbc6ce2dde56ea2e0d167e10b698e9c
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-05 10:40:36 +0530

    SDE_1_Day_1

--- 2026/SDE_Sheet/Day_1/73_Set_Matrix_Zeroes.cpp
+++ 2026/SDE_Sheet/Day_1/73_Set_Matrix_Zeroes.cpp
@@ -55,3 +55,62 @@ public:
 };
 
 //Optimal Approach
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRowImpacted = false;
+        bool firstColumnImpacted = false;
+
+        //T.C = O(m*n)
+        //S.C = O(1), Constant Space
+
+        //check first row is impacted or not
+        for(int col = 0; col < n; col++) {
+            if(matrix[0][col] == 0) {
+                firstRowImpacted = true;
+                break;
+            }
+        }
+
+        //check first column is impacted or not
+        for(int row = 0; row < m; row++) {
+            if(matrix[row][0] == 0) {
+                firstColumnImpacted = true;
+                break;
+            }
+        }
+        
+        //set markers in the first row/column
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0; //column
+                    matrix[0][j] = 0; //row
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowImpacted) {
+            for(int col = 0; col < n; col++) {
+                matrix[0][col] = 0;
+            }
+        }
+        
+        if(firstColumnImpacted) {
+            for(int row = 0; row < m; row++) {
+                matrix[row][0] = 0;
+            }
+        }
+        
+    }
+};
