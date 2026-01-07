0a807bbe8f4b7fabebc212c366a1bfe6119d37dd
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-07 09:36:51 +0530

    DFS Approach

--- 2026/January/Day_6/1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
+++ 2026/January/Day_6/1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -48,3 +48,57 @@ public:
 //DFS Approach
 //Time Complexity: O(N) where N is the number of nodes in the binary tree
 //Space Complexity: O(H) where H is the height of the binary tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int maxSum = INT_MIN;
+        int resultLevel = 0;
+
+        queue<TreeNode*> que;
+        que.push(root);
+
+        int currentLevel = 1;
+
+        while(!que.empty()) {
+            int n = que.size();
+
+            int sum = 0;
+
+            //Traversing all elements of Current Level
+            while(n--) {
+                TreeNode* temp = que.front();
+                que.pop();
+
+                sum += temp->val;
+
+                if(temp->left) {
+                    que.push(temp->left);
+                }
+
+                if(temp->right) {
+                    que.push(temp->right);
+                }
+            }
+
+            if(sum > maxSum) {
+                maxSum = sum;
+                resultLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return resultLevel;
+    }
+};
