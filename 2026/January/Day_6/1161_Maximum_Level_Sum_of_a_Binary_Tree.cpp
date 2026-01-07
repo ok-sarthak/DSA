39ab72611b31945e387ccc52df4764ed15d9ace0
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-07 09:46:05 +0530

    Fixed

--- 2026/January/Day_6/1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
+++ 2026/January/Day_6/1161_Maximum_Level_Sum_of_a_Binary_Tree.cpp
@@ -61,44 +61,36 @@ public:
  */
 class Solution {
 public:
+
+    map<int, int> mp;
+
+    //InOrder DFS
+    void DFS(TreeNode* root, int level) {
+        if(!root) return;
+
+        mp[level] += root->val;
+
+        DFS(root->left, level+1);
+        DFS(root->right, level+1);
+    }
+
     int maxLevelSum(TreeNode* root) {
+        mp.clear();
+
+        DFS(root, 1);
+
         int maxSum = INT_MIN;
         int resultLevel = 0;
 
-        queue<TreeNode*> que;
-        que.push(root);
-
-        int currentLevel = 1;
-
-        while(!que.empty()) {
-            int n = que.size();
-
-            int sum = 0;
-
-            //Traversing all elements of Current Level
-            while(n--) {
-                TreeNode* temp = que.front();
-                que.pop();
-
-                sum += temp->val;
-
-                if(temp->left) {
-                    que.push(temp->left);
-                }
-
-                if(temp->right) {
-                    que.push(temp->right);
-                }
-            }
+        for(auto &i : mp) {
+            int level = i.first;
+            int sum = i.second;
 
             if(sum > maxSum) {
                 maxSum = sum;
-                resultLevel = currentLevel;
+                resultLevel = level;
             }
-
-            currentLevel++;
         }
-
         return resultLevel;
     }
 };
