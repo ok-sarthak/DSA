ab3de929092aff1f62455ef1923962dc6b81f06f
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-09 11:27:14 +0530

    Day_9_865

---
+++ 2026/January/Day_9/865_Smallest_Subtree_with_all_the_Deepest_Nodes.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    unordered_map<int, int> mp;
+    int maxD = 0;
+
+    TreeNode* LCA(TreeNode* root) {
+        if(root == NULL || mp[root->val] == maxD) {
+            return root;
+        }
+
+        TreeNode* l = LCA(root->left);
+        TreeNode* r = LCA(root->right);
+
+        if(l && r) {
+            return root;
+        }
+
+        return l != NULL ? l : r;
+    }
+
+    void depth(TreeNode* root, int d) {
+        if(root == NULL) return;
+
+        maxD = max(maxD, d);
+
+        mp[root->val] = d;
+        depth(root->left, d+1);
+        depth(root->right, d+1);
+    }
+
+    //T.C = O(2N) = O(N), S.C = O(maxD)
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        depth(root, 0);
+
+        return LCA(root);
+    }
+};
