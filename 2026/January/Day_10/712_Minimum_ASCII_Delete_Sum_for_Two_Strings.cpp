4d4c9d72a1442e6171c64be86faadcf215b0a585
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-10 08:26:27 +0530

    Day_10_712

---
+++ 2026/January/Day_10/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    int m,n;
+
+    int t[1001][1001];
+
+    int solve(int i, int j, string &s1, string &s2) {
+        if(i >= m && j >= n) return 0;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(i >= m) {
+            return t[i][j] = s2[j] + solve(i, j+1, s1, s2);
+        } else if(j >= n) {
+            return t[i][j] = s1[i] + solve(i+1, j, s1, s2);
+        }
+
+        if(s1[i] == s2[j]) {
+            return t[i][j] = solve(i+1, j+1, s1, s2);
+        }
+
+        int del_s1_i = s1[i] + solve(i+1, j, s1, s2);
+        int del_s2_j = s2[j] + solve(i, j+1, s1, s2);
+
+        return t[i][j] = min(del_s1_i, del_s2_j);
+    }
+
+    int minimumDeleteSum(string s1, string s2) {
+        m = s1.size();
+        n = s2.size();
+
+        memset(t, -1, sizeof(t));
+
+        return solve(0, 0, s1, s2);
+    }
+};
