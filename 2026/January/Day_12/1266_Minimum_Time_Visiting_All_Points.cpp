09238d9a4a5928a36d94a2027c19f8414bbceed4
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-01-12 11:10:12 +0530

    Day_12_1266

---
+++ 2026/January/Day_12/1266_Minimum_Time_Visiting_All_Points.cpp
@@ -0,0 +1,25 @@
+//T.C O(N)
+//S.C O(1)
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        int steps = 0;
+
+        for(int i = 0; i < n-1; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
+
+            int dx = abs(x1 - x2);
+            int dy = abs(y1 - y2);
+
+            steps += min(dx, dy) + abs(dx - dy);
+        }
+
+        return steps;
+    }
+};
