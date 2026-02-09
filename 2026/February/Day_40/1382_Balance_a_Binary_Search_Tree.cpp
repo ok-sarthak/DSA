70f6506d5ba8c590b8ec2c3dbdace4acdc3873ac
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-02-09 10:40:24 +0530

    Day_40_1382

---
+++ 2026/February/Day_40/1382_Balance_a_Binary_Search_Tree.cpp
@@ -0,0 +1,39 @@
+//T.C: O(N)
+//S.C: O(N)
+class Solution {
+public:
+
+    void inOrder(TreeNode* root, vector<int>& vec) {
+        if(!root)
+            return;
+
+        inOrder(root->left, vec);
+        vec.push_back(root->val);
+        inOrder(root->right, vec);
+    }
+
+    TreeNode* construct(int l, int r, vector<int>& vec) {
+        if(l > r)
+            return NULL;
+        
+        int mid = l + (r-l)/2;
+
+        TreeNode* root = new TreeNode(vec[mid]);
+
+        root->left = construct(l, mid-1, vec);
+        root->right = construct(mid+1, r, vec);
+
+        return root;
+    }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> vec;
+
+        inOrder(root, vec);
+
+        int l = 0;
+        int r = vec.size()-1;
+
+        return construct(l, r, vec);
+    }
+};
