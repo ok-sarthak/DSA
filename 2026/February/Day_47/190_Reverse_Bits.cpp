719b27e2544e89a0e367b272875856d8603870ab
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-02-16 10:00:26 +0530

    Day_47_190

---
+++ 2026/February/Day_47/190_Reverse_Bits.cpp
@@ -0,0 +1,21 @@
+//T.C : O(1)
+//S.C : O(1)
+class Solution {
+public:
+    int reverseBits(int n) {
+        if(n == 0)
+            return 0;
+        
+        int result = 0;
+
+        for(int i = 1; i <= 32; i++) {
+            result <<= 1;
+
+            result = (result | (n & 1));
+
+            n >>= 1;
+        }
+
+        return result;
+    }
+};
