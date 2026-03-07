10b0c2d10ec865e96934b275cf005726c714aef3
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-03-08 00:02:42 +0530

    Day_66_1888

--- 2026/March/Day_66/1888_Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating.cpp
+++ 2026/March/Day_66/1888_Minimum_Number_of_Flips_to_Make_the_Binary_String_Alternating.cpp
@@ -1,3 +1,90 @@
+//Approach - 1: Sliding Window + modifying input s = s+s
+//T.C : O(2*n) ~= O(n)
+//S.C : O(2*n) for s1 and s2 and s+=s
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.length();
+
+        s += s;
+
+        string s1, s2;
+
+        for(int i = 0; i < 2*n; i++) {
+            s1 += (i%2 == 0) ? '0' : '1';
+            s2 += (i%2 == 0) ? '1' : '0';
+        }
+
+        int f1 = 0, f2 = 0;
+
+        int result = INT_MAX;
+
+        int i = 0, j = 0;
+
+        while(j < 2*n) {
+            if(s[j] != s1[j]) f1++;
+            if(s[j] != s2[j]) f2++;
+
+            while(j-i+1 > n) {
+                if(s[i] != s1[i]) f1--;
+                if(s[i] != s2[i]) f2--;
+
+                i++;
+            }
+
+            if(j-i+1 == n) result = min({result, f1, f2});
+
+            j++;
+        }
+
+        return result;
+    }
+};
+
+
+//Approach - 2:Sliding Window + without modifying input
+//T.C : O(2*n) ~= O(n)
+//S.C : O(2*n) for s1 and s2
+class Solution {
+public:
+    int minFlips(string s) {
+        int n = s.length();
+
+        string s1, s2;
+
+        for(int i = 0; i < 2*n; i++) {
+            s1 += (i%2 == 0) ? '0' : '1';
+            s2 += (i%2 == 0) ? '1' : '0';
+        }
+
+        int f1 = 0, f2 = 0;
+
+        int result = INT_MAX;
+
+        int i = 0, j = 0;
+
+        while(j < 2*n) {
+            if(s[j%n] != s1[j]) f1++;
+            if(s[j%n] != s2[j]) f2++;
+
+            while(j-i+1 > n) {
+                if(s[i%n] != s1[i]) f1--;
+                if(s[i%n] != s2[i]) f2--;
+
+                i++;
+            }
+
+            if(j-i+1 == n) result = min({result, f1, f2});
+
+            j++;
+        }
+
+        return result;
+    }
+};
+
+
+//Approach - 3: Sliding Window + without modifying input
 //T.C: O(n)
 //S.C: O(1)
 class Solution {
