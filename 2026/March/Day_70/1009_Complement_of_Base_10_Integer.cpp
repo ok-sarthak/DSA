3b29bd43be25396647604931185a7c120f20fb57
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-03-11 23:30:51 +0530

    Day_70_1009

---
+++ 2026/March/Day_70/1009_Complement_of_Base_10_Integer.cpp
@@ -0,0 +1,57 @@
+//Approach - 1
+// T.C: O(log(n))
+// S.C: O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+
+        int counter = 0;
+        int result = 0;
+
+        while(n) {
+            int r = n%2;
+            result += (pow(2, counter) * !r);
+            counter++;
+            n = n >> 1; // n /= 2; 
+        }
+
+        return result;
+    }
+};
+
+
+//Approach - 2
+// T.C: O(log(n))
+// S.C: O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+
+        int mask = 1;
+
+        while(mask < n) {
+            mask = (mask << 1) | 1;
+        }
+
+        return mask ^ n;
+    }
+};
+
+
+//Approach - 3
+// T.C: O(1)
+// S.C: O(1)
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n == 0) return 1;
+
+        int bits = floor(log2(n)) + 1;
+        
+        int mask = (1 << bits) - 1; //pow(2, bits) - 1;
+
+        return mask ^ n;
+    }
+};
