bc0a613dd43c0aa5784475b96ecf8af5f0fe4f88
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-03-14 23:44:35 +0530

    Day_73_1415

--- 2026/March/Day_73/1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
+++ 2026/March/Day_73/1415_The_k-th_Lexicographical_String_of_All_Happy_Strings_of_Length_n.cpp
@@ -1,3 +1,4 @@
+//Approach - 1: Backtracking template - storing all possible strings
 //T.C: O(n * 3 * 2^(n-1)) ~= O(n*2^n)
 //S.C: O(n * 2^n) , total 2^n strings eaching having length n
 class Solution {
@@ -30,5 +31,46 @@ public:
         if(result.size() < k) return "";
 
         return result[k-1];
+    }
+};
+
+
+//Approach - 2: Khandani Backtracking template - Without storing all possible strings
+//T.C : O(n * 3 * 2^(n-1)) ~= O(n*2^n)
+//S.C : O(n) , by recursion system 
+class Solution {
+public:
+
+    void solve(int n, string &curr, int &count, int k, string &result) {
+        if(curr.length() == n) {
+            count++;
+
+            if(count == k) {
+                result = curr;
+            }
+            return;
+        }
+
+        for(char ch = 'a'; ch <= 'c'; ch++) {
+            if(!curr.empty() && curr.back() == ch) continue;
+
+            curr.push_back(ch);
+
+            solve(n, curr, count, k, result);
+
+            if(!result.empty()) return;
+
+            curr.pop_back();
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        string curr = "";
+        string result = "";
+        int count = 0;
+
+        solve(n, curr, count, k, result);
+
+        return result;
     }
 };
