43aa149b3b8f642e6b4e7322642fd14615148232
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-04-05 22:51:30 +0530

    Day_95_657

---
+++ 2026/March/Day_95/657_Robot_Return_to_Origin.cpp
@@ -0,0 +1,22 @@
+//Approach: Simple simulation
+//T.C: O(n)
+//S.C: O(1)
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+
+        for(char &ch : moves) {
+            if(ch == 'U') y++;
+
+            else if(ch == 'D') y--;
+
+            else if(ch == 'L') x--;
+
+            else if(ch == 'R') x++;
+        }
+
+        return x == 0 && y == 0;
+    }
+};
