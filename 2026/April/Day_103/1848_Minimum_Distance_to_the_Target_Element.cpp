e81a5d19cb90245e5f3c5fe0b73b089fed44f73b
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-04-13 23:09:14 +0530

    Day_103_1848

---
+++ 2026/April/Day_103/1848_Minimum_Distance_to_the_Target_Element.cpp
@@ -0,0 +1,42 @@
+//Approach - 1: Simple simulation
+//T.C: O(n)
+//S.C: O(1)
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int n = nums.size();
+
+        int result = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+
+            if(nums[i] == target) {
+                result = min(result, abs(i - start));
+            }
+        }
+
+        return result;
+    }
+};
+
+
+//Approach - 2: Loop with Early Break
+//T.C: O(n)
+//S.C: O(1)
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int n = nums.size();
+
+        int result = INT_MAX;
+
+        for(int i = 0; i < n && result > abs(i - start); i++) {
+
+            if(nums[i] == target) {
+                result = min(result, abs(i - start));
+            }
+        }
+
+        return result;
+    }
+};
