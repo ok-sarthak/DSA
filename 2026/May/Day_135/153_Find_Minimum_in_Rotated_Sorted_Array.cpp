122b1b7536a6cbfcbbafbb6aebbc334b4c1c8670
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-05-15 23:09:05 +0530

    Day_135_153

---
+++ 2026/May/Day_135/153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,28 @@
+//Approach: Binary Search
+//T.C: O(log n)
+//S.C: O(1)
+class Solution {
+public:
+    int findPivot(vector<int>& nums) {
+        int mid;
+
+        int l = 0, r = nums.size()-1;
+        
+        while(l < r) {
+            mid = l + (r-l)/2;
+          
+            if(nums[mid] > nums[r]) {
+                l = mid+1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return nums[l];
+    }
+    
+    int findMin(vector<int>& nums) {
+        
+        return findPivot(nums);
+    }
+};
