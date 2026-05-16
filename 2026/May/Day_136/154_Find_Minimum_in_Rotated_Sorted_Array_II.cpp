903889a7b3ffe05f75499c7c0a1981fb6b211d13
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-05-16 22:53:49 +0530

    Day_136_154

---
+++ 2026/May/Day_136/154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,37 @@
+//Approach: Binary Search with Duplicate Elimination
+//T.C: O(log n) , on average & O(n) in worst case due to duplicate skipping
+//S.C: O(1)
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        
+        int r = nums.size() - 1;
+
+        int resultIdx = 0;
+
+        while(l <= r) {
+
+            while(l < r && nums[l] == nums[l + 1])
+                l++;
+
+            while(l < r && nums[r] == nums[r - 1])
+                r--;
+
+            int mid = l + (r - l) / 2;
+
+            if(nums[mid] < nums[resultIdx])
+                resultIdx = mid;
+
+            if(nums[mid] > nums[r]) {
+                l = mid + 1;
+            }
+
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return nums[resultIdx];
+    }
+};
