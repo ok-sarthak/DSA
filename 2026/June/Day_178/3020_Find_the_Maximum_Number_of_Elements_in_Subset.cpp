21a9c5f4a524f0f8e8583fc79e64fdff1b259f89
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-06-27 23:56:53 +0530

    Day_178_3020

---
+++ 2026/June/Day_178/3020_Find_the_Maximum_Number_of_Elements_in_Subset.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    typedef long long ll;
+
+    int maximumLength(vector<int>& nums) {
+        unordered_map<ll, int> mp;
+
+        for(int &num : nums) {
+            mp[num]++;
+        }
+
+        int result = 0;
+
+        if(mp[1]%2) {
+            result = mp[1];
+        } else {
+            result = mp[1]-1;
+        }
+
+        for(auto &[num, _] : mp) {
+            if(num == 1) continue;
+
+            ll curr = num;
+            int len = 0;
+
+            while(mp.count(curr) && mp[curr] > 1) {
+                len += 2;
+                curr = curr*curr;
+            }
+
+            if(mp.count(curr) == 1) {
+                len += 1;
+            } else {
+                len -= 1;
+            }
+
+            result = max(result, len);
+        }
+
+        return result;
+    }
+};
