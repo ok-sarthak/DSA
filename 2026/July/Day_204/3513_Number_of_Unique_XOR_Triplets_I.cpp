f1064964ea3e3b98d59e0ed5a32d049993df53e2
Sarthak Chakraborty <csarthakcal@gmail.com>
2026-07-23 23:55:25 +0530

    Day_204_3513

---
+++ 2026/July/Day_204/3513_Number_of_Unique_XOR_Triplets_I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1 || n == 2)
+            return n;
+        
+        int ans = 1;
+        
+        while(ans <= n) {
+            ans = (ans << 1);
+        }
+
+        return ans;
+    }
+};
